644f08d6c107edfdbaad6ec61918d251af37e205
ehernandez-i090l <ehernandez-i090l@kinal.edu.gt>
2022-03-06 13:34:36 -0600

    Definir Profesor

---
+++ entities/Profesor.js
@@ -0,0 +1,35 @@
+import AbstractPersona from "./AbstractPersona.js";
+
+class Profesor extends AbstractPersona{
+    #cui = new String();
+    #cargo = new String();
+
+    constructor(_apellidos, _nombres, _email, _cui, _cargo){
+        super(_apellidos, _nombres, _email);
+        this.#cui = _cui;
+        this.#cargo = _cargo;
+    }
+
+    setCui(_cui){
+        this.#cui = _cui;
+    }
+
+    setCargo(_cargo){
+        this.#cargo = _cargo;
+    }
+
+    getCui(){
+        return this.#cui;
+    }
+
+    getCargo(){
+        return this.#cargo;
+    }
+
+    //@Override
+    tomarAsistencia(){
+        return 2
+    }
+}
+
+export default Profesor
